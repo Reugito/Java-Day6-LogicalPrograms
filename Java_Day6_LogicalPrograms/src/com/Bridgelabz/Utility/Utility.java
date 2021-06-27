51c765b50aa0464f339329a067473291cd03384f
Reugito <raosahebdhotreedu@gmail.com>
2021-06-27 12:03:20 +0530

    added jUnit programs

--- Java_Day6_LogicalPrograms/src/com/Bridgelabz/Utility/Utility.java
+++ Java_Day6_LogicalPrograms/src/com/Bridgelabz/Utility/Utility.java
@@ -119,4 +119,150 @@ public class Utility {
        double elaps = ((stop - start)/1000)%60;
        System.out.println("Elaps time = "+elaps+" sec");	
 	}
+	/* method to get rupee note count */
+	
+	static int i=0, total = 0;
+	public static int getRupeeNotesCount(int amount, int[] notes) {
+		int remainder = 0;
+		if(amount == 0) {
+			System.out.println("Invalid amount");
+			return 0;
+		}
+		else {
+			if(amount >= notes[i]) {
+				int calculateNotes = amount / notes[i];
+				remainder = amount % notes[i];
+				amount = remainder;
+				total += calculateNotes;
+				System.out.println(notes[i] + ".Rs notes "+calculateNotes);
+				
+			}
+		}
+		i++;
+		return getRupeeNotesCount(amount, notes);
+	}
+	/* method to get date  */
+	static int day=0, month=0, year=0;
+	public void getDate() {
+		
+		do {
+			System.out.print("Enter the day between 1 to 31 : ");
+			day = getIntValue();
+			if(day > 31 || day <= 0) {
+				System.out.println("Invalid day: "+day);
+			}
+		}while(day > 31 || day <= 0 );
+			
+		
+		do {
+			System.out.print("Enter month from 1 to 12 : ");
+			month = getIntValue();
+			if(month > 12 || month <= 0)
+				System.out.println("Invalid month: "+month);
+		}while(month > 12 || month <= 0);
+		
+		int digits = 0, temp;
+		do {
+			
+			System.out.print("Enter the four digit year : ");
+			year = getIntValue();
+			if(year > 0) {
+				digits = 0;
+				temp = year;
+				while(temp != 0) {
+					digits++;
+					temp /= 10;	
+				}
+			}
+			if(digits != 4)
+				System.out.println("Invalid year ");
+		}while(digits != 4);
+	}
+	
+	/* method to get day of weeks  */
+	public static void getDayofWeek() {
+		System.out.format("Date: %d/ %d/ %d",day,month,year);
+		int y0, d0, m0, x;
+		y0 = year - (14 - month) / 12;
+		x = y0 + y0/4 - y0/100 + y0/400;
+		m0 = month + 12 * ((14 - month) / 12) - 2;
+		d0 = (day + x + 31*m0 / 12) % 7;
+		System.out.println();
+		switch(d0) {
+		case 0:
+			System.out.println("sunday");
+			break;
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		default:
+			System.out.println("Saturday");
+		}
+	}
+	
+	// method for temperature conversion
+
+	public static void getTempreture() {
+		float Celsius, Fahrenheit;
+		int choice=0;
+		boolean flag = false;
+		System.out.println("1. Tempreture in Celsius ");
+        System.out.println("2. Tempreture in  fahrenheit");
+        do {
+            System.out.print("\nselect the choice to enter the tempreture type: ");
+            Utility utility = new Utility();
+            choice = utility.getIntValue();
+            switch(choice) {
+       			case 1:
+       				System.out.print("Enter tempreture in Celsius: ");
+       				Celsius = utility.getIntValue();
+       				Fahrenheit = (Celsius * 9/5) + 32;
+       				System.out.println("Tempreture = "+Fahrenheit+" °F");
+       				flag = false;
+       				break;
+       			case 2:
+       				System.out.print("Enter tempreture in Fahrenheit: ");
+       				Fahrenheit = utility.getIntValue();
+       				Celsius = (Fahrenheit - 32) * 5/9;
+       				System.out.format("Tempreture = "+Celsius+" °C");
+       				flag = false;
+       				break;
+       			default:
+       				flag = true;
+       				System.out.print("Invalid selection");
+            }
+       }while(flag);	
+	}
+
+	// get the monthly payment
+	public static void getMonthlyPayment(int years, float P, float R) {
+		int n = years * 12;     
+        float r = R / (12*100);
+        float payment = (P*r) / (float)(1-(Math.pow(1+r, -n)));
+		System.out.println("Monthly payement  ="+payment);
+	}
+	
+	// method to get square root
+	public static void sqrt(int c) {
+        double epsilon = 1e-15;
+        float t = c;
+        while(Math.abs(t-c/t) > epsilon * t) {
+            t = (float) ((c/t + t)/2);     
+            }
+        System.out.println("Square root of " + c + " = " + t);
+	}
+	
+
 }
